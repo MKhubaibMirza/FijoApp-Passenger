d839230bcb03853b1391ce7ddd7d6ded8d57b43b
MKhubaibMirza <72186922+MKhubaibMirza@users.noreply.github.com>
2020-12-09 14:37:07 +0500

    2 More pages are done

--- src/app/add-payment-method/add-payment-method.page.ts
+++ src/app/add-payment-method/add-payment-method.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { SureToCancelPaymentMethodPage } from '../sure-to-cancel-payment-method/sure-to-cancel-payment-method.page';
 
 @Component({
   selector: 'app-add-payment-method',
@@ -7,14 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddPaymentMethodPage implements OnInit {
 
-  constructor() { }
+  constructor(public modal: ModalController) { }
 
   ngOnInit() {
   }
   card = {
-    number: ''
+    number: '',
+    date: '',
+    cvv: ''
   }
-  backBtn() {
-
+  save() {
+    console.log(this.card);
+  }
+  async backBtn() {
+    const modal = await this.modal.create({
+      component: SureToCancelPaymentMethodPage,
+      cssClass: 'sureToCancelPaymentMethod'
+    });
+    return await modal.present();
   }
 }
