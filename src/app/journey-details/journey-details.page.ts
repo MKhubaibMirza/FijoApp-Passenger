2277dc4e56b9bdcb504566671806a5e2c4f7ac31
MKhubaibMirza <72186922+MKhubaibMirza@users.noreply.github.com>
2020-12-11 11:00:09 +0500

    Small Changes in Journey Detail Page

--- src/app/journey-details/journey-details.page.ts
+++ src/app/journey-details/journey-details.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-journey-details',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JourneyDetailsPage implements OnInit {
 
-  constructor() { }
+  constructor(public nav: NavController) { }
 
   ngOnInit() {
   }
-
+  back() {
+    this.nav.back();
+  }
 }
